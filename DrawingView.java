bb369f19bdb1c42c1f55dbbf9514956f4163a76f
dong1016 <donghe2006@gmail.com>
2017-10-17 20:57:24 -0700

    Finalized app
    Fixed bugs, added multitouch, added color swapping, added clear, hopefully no files missing

--- DrawingView.java
+++ DrawingView.java
@@ -18,13 +18,13 @@ import java.util.HashMap;
 
 public class DrawingView extends View {
 
-    private Path drawPath;
     private Paint drawPaint, canvasPaint;
     private int defaultColor = 0xFF000000;
-    private Canvas drawCanvas;
+    public Canvas drawCanvas;
     private Bitmap canvasBitmap;
 
     private HashMap<Integer, PointF> fingerPos = new HashMap<>();
+    private HashMap<Integer, Path> fingerPaths = new HashMap<>();
 
     public DrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -32,7 +32,8 @@ public class DrawingView extends View {
     }
 
     private void setupDrawing() {
-        drawPath = new Path();
+        canvasBitmap = Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
         drawPaint = new Paint();
         drawPaint.setColor(defaultColor);
         drawPaint.setAntiAlias(true);
@@ -46,38 +47,68 @@ public class DrawingView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        if(w > h) {
-            canvasBitmap = Bitmap.createBitmap(w, w, Bitmap.Config.ARGB_8888);
-        }else if(h > w){
-            canvasBitmap = Bitmap.createBitmap(h, h, Bitmap.Config.ARGB_8888);
-        }else{
-            canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        int new_max_size = Math.max(w, h);
+        if(canvasBitmap.getWidth() < new_max_size) {
+            Bitmap canvasBitmapNew = Bitmap.createBitmap(new_max_size, new_max_size, Bitmap.Config.ARGB_8888);
+            drawCanvas.setBitmap(canvasBitmapNew);
+            drawCanvas.drawBitmap(canvasBitmap, 0, 0, null);
+            canvasBitmap = canvasBitmapNew;
+
         }
-        drawCanvas = new Canvas(canvasBitmap);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
         canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
-        canvas.drawPath(drawPath, drawPaint);
+        for(int i : fingerPos.keySet()){
+            PointF point = fingerPos.get(i);
+            Path tPath = fingerPaths.get(i);
+            if(((point != null) && (tPath != null))){
+
+                if(i < 4){
+                    drawPaint.setColor(MainActivity.colors[MainActivity.temp[i]]);
+                }else{
+                    drawPaint.setColor(defaultColor);
+                }
+
+                tPath.lineTo(point.x, point.y);
+                drawCanvas.drawPath(tPath, drawPaint);
+            }
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float xPos = event.getX();
-        float yPos = event.getY();
-        switch (event.getAction()) {
+
+        int pointerIndex = event.getActionIndex();
+        int pointerId = event.getPointerId(pointerIndex);
+        int action = event.getActionMasked();
+
+        switch (action) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_POINTER_DOWN:
-                drawPath.moveTo(xPos, yPos);
+                PointF point = new PointF();
+                point.x = event.getX(pointerIndex);
+                point.y = event.getY(pointerIndex);
+                Path tempP = new Path();
+                tempP.moveTo(point.x, point.y);
+                fingerPos.put(pointerId, point);
+                fingerPaths.put(pointerId, tempP);
                 break;
             case MotionEvent.ACTION_MOVE:
-                drawPath.lineTo(xPos, yPos);
+                for(int i = 0; i < event.getPointerCount(); i++){
+                    PointF curPoint = fingerPos.get(event.getPointerId(i));
+                    if(curPoint != null){
+                        curPoint.x = event.getX(i);
+                        curPoint.y = event.getY(i);
+                    }
+                }
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_POINTER_UP:
-                drawCanvas.drawPath(drawPath, drawPaint);
-                drawPath.reset();
+                fingerPos.remove(pointerId);
+                fingerPaths.remove(pointerId);
                 break;
             default:
                 return false;
--- MainActivity.java
+++ MainActivity.java
@@ -1,24 +1,35 @@
 package edu.ucsb.he.dong.dhedrawingmultitouch;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.MenuItemHoverListener;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawingView drawable;
+
+    public static int[] colors;
+    public static int[] temp = {0, 1, 2, 3};
+
+    public int[] helper(){
+        return temp;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawable = (DrawingView)findViewById(R.id.drawing);
+        Resources res = getResources();
+        colors = res.getIntArray(R.array.paint_colors);
+
     }
 
     @Override
@@ -31,10 +42,54 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         if(item.getItemId() == R.id.action_clear){
-            //clear stuff
+            drawable = (DrawingView)findViewById(R.id.drawing);
+            drawable.drawCanvas.drawARGB(255, 255, 255, 255);
         }
         return super.onOptionsItemSelected(item);
     }
 
+    public void changeColor1(View view){
+        Button x = (Button)findViewById(R.id.button1);
+        Random r = new Random();
+        int result = r.nextInt(24);
+        while(result == temp[1] || result == temp[2] || result == temp[3]){
+            result = r.nextInt(24);
+        }
+        temp[0] = result;
+        x.setBackgroundColor(colors[temp[0]]);
+    }
+
+    public void changeColor2(View view){
+        Button x = (Button)findViewById(R.id.button2);
+        Random r = new Random();
+        int result = r.nextInt(24);
+        while(result == temp[0] || result == temp[2] || result == temp[3]){
+            result = r.nextInt(24);
+        }
+        temp[1] = result;
+        x.setBackgroundColor(colors[temp[1]]);
+    }
+
+    public void changeColor3(View view){
+        Button x = (Button)findViewById(R.id.button3);
+        Random r = new Random();
+        int result = r.nextInt(24);
+        while(result == temp[0] || result == temp[1] || result == temp[3]){
+            result = r.nextInt(24);
+        }
+        temp[2] = result;
+        x.setBackgroundColor(colors[temp[2]]);
+    }
+
+    public void changeColor4(View view){
+        Button x = (Button)findViewById(R.id.button4);
+        Random r = new Random();
+        int result = r.nextInt(24);
+        while(result == temp[0] || result == temp[1] || result == temp[2]){
+            result = r.nextInt(24);
+        }
+        temp[3] = result;
+        x.setBackgroundColor(colors[temp[3]]);
+    }
 
 }
